53ab35908c94fb0fdfa81477169252705e968687
AlekseyKS54 <aleksey.ks54@yandex.ru>
2026-03-20 02:06:00 +0300

    Добавлен CORS и установлен context-path для сервера

--- src/main/java/com/myblog/MyBlogBackAppApplication.java
+++ src/main/java/com/myblog/MyBlogBackAppApplication.java
@@ -2,7 +2,10 @@ package com.myblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJdbcRepositories
@@ -12,4 +15,13 @@ public class MyBlogBackAppApplication {
         SpringApplication.run(MyBlogBackAppApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }

c22713d3abe37b128cb9464dff2c1dbe22c18d62
AlekseyKS54 <aleksey.ks54@yandex.ru>
2026-03-20 03:04:22 +0300

    Реализован тест репозитория Post вместо теста DAO

--- src/test/java/com/myblog/repository/PostRepositoryTest.java
+++ src/test/java/com/myblog/repository/PostRepositoryTest.java
@@ -1,16 +1,12 @@
-package com.myblog.dao;
+package com.myblog.repository;
 
-import com.myblog.config.DatabaseConfig;
-import com.myblog.dao.impl.PostDaoImpl;
-import com.myblog.dao.impl.TagDaoImpl;
+import com.myblog.MyBlogBackAppApplication;
 import com.myblog.model.Post;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
@@ -18,14 +14,16 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(SpringExtension.class)
-@ContextConfiguration(classes = {DatabaseConfig.class, PostDaoImpl.class, TagDaoImpl.class})
+@SpringBootTest(classes = MyBlogBackAppApplication.class)
 @Transactional
-class PostDaoIntegrationTest {
+public class PostRepositoryTest {
 
     @Autowired
-    private PostDao postDao;
+    private PostRepository postRepository;
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
@@ -48,14 +46,14 @@ class PostDaoIntegrationTest {
         post.setTags(Arrays.asList("tag1", "tag2"));
 
         // When
-        Post createdPost = postDao.create(post);
+        Post createdPost = postRepository.create(post);
 
         // Then
         assertNotNull(createdPost.getId());
         assertEquals("Test Post", createdPost.getTitle());
         assertEquals(0, createdPost.getLikesCount());
-        
-        Optional<Post> foundPost = postDao.findById(createdPost.getId());
+
+        Optional<Post> foundPost = postRepository.findById(createdPost.getId());
         assertTrue(foundPost.isPresent());
         assertEquals(createdPost.getId(), foundPost.get().getId());
         assertEquals(2, foundPost.get().getTags().size());
@@ -68,16 +66,16 @@ class PostDaoIntegrationTest {
         post1.setTitle("First Post");
         post1.setText("First content");
         post1.setTags(Arrays.asList("java"));
-        postDao.create(post1);
+        postRepository.create(post1);
 
         Post post2 = new Post();
         post2.setTitle("Second Post");
         post2.setText("Second content");
         post2.setTags(Arrays.asList("spring"));
-        postDao.create(post2);
+        postRepository.create(post2);
 
         // When
-        List<Post> posts = postDao.findAll("", 1, 10);
+        List<Post> posts = postRepository.findAll("", 1, 10);
 
         // Then
         assertEquals(2, posts.size());
@@ -90,16 +88,16 @@ class PostDaoIntegrationTest {
         post1.setTitle("Java Tutorial");
         post1.setText("Content");
         post1.setTags(Arrays.asList());
-        postDao.create(post1);
+        postRepository.create(post1);
 
         Post post2 = new Post();
         post2.setTitle("Spring Framework");
         post2.setText("Content");
         post2.setTags(Arrays.asList());
-        postDao.create(post2);
+        postRepository.create(post2);
 
         // When
-        List<Post> posts = postDao.findAll("Java", 1, 10);
+        List<Post> posts = postRepository.findAll("Java", 1, 10);
 
         // Then
         assertEquals(1, posts.size());
@@ -113,16 +111,16 @@ class PostDaoIntegrationTest {
         post1.setTitle("Post 1");
         post1.setText("Content");
         post1.setTags(Arrays.asList("java", "spring"));
-        postDao.create(post1);
+        postRepository.create(post1);
 
         Post post2 = new Post();
         post2.setTitle("Post 2");
         post2.setText("Content");
         post2.setTags(Arrays.asList("python"));
-        postDao.create(post2);
+        postRepository.create(post2);
 
         // When
-        List<Post> posts = postDao.findAll("#java", 1, 10);
+        List<Post> posts = postRepository.findAll("#java", 1, 10);
 
         // Then
         assertEquals(1, posts.size());
@@ -136,13 +134,13 @@ class PostDaoIntegrationTest {
         post.setTitle("Original Title");
         post.setText("Original content");
         post.setTags(Arrays.asList("tag1"));
-        Post createdPost = postDao.create(post);
+        Post createdPost = postRepository.create(post);
 
         // When
         createdPost.setTitle("Updated Title");
         createdPost.setText("Updated content");
         createdPost.setTags(Arrays.asList("tag2", "tag3"));
-        Post updatedPost = postDao.update(createdPost);
+        Post updatedPost = postRepository.update(createdPost);
 
         // Then
         assertEquals("Updated Title", updatedPost.getTitle());
@@ -157,13 +155,13 @@ class PostDaoIntegrationTest {
         post.setTitle("Test Post");
         post.setText("Test content");
         post.setTags(Arrays.asList());
-        Post createdPost = postDao.create(post);
+        Post createdPost = postRepository.create(post);
 
         // When
-        postDao.delete(createdPost.getId());
+        postRepository.delete(createdPost.getId());
 
         // Then
-        Optional<Post> foundPost = postDao.findById(createdPost.getId());
+        Optional<Post> foundPost = postRepository.findById(createdPost.getId());
         assertFalse(foundPost.isPresent());
     }
 
@@ -174,14 +172,14 @@ class PostDaoIntegrationTest {
         post.setTitle("Test Post");
         post.setText("Test content");
         post.setTags(Arrays.asList());
-        Post createdPost = postDao.create(post);
+        Post createdPost = postRepository.create(post);
 
         // When
-        postDao.incrementLikes(createdPost.getId());
-        postDao.incrementLikes(createdPost.getId());
+        postRepository.incrementLikes(createdPost.getId());
+        postRepository.incrementLikes(createdPost.getId());
 
         // Then
-        Optional<Post> updatedPost = postDao.findById(createdPost.getId());
+        Optional<Post> updatedPost = postRepository.findById(createdPost.getId());
         assertTrue(updatedPost.isPresent());
         assertEquals(2, updatedPost.get().getLikesCount());
     }
@@ -193,25 +191,24 @@ class PostDaoIntegrationTest {
         post1.setTitle("Post 1");
         post1.setText("Content");
         post1.setTags(Arrays.asList());
-        postDao.create(post1);
-        
+        postRepository.create(post1);
+
         Post post2 = new Post();
         post2.setTitle("Post 2");
         post2.setText("Content");
         post2.setTags(Arrays.asList());
-        postDao.create(post2);
-        
+        postRepository.create(post2);
+
         Post post3 = new Post();
         post3.setTitle("Post 3");
         post3.setText("Content");
         post3.setTags(Arrays.asList());
-        postDao.create(post3);
+        postRepository.create(post3);
 
         // When
-        int count = postDao.getTotalCount("");
+        int count = postRepository.getTotalCount("");
 
         // Then
         assertEquals(3, count);
     }
 }
-
